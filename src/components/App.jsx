d4716f393dc1d720b8b93156284844e9ce24e316
Thierry Deo <thierry.deo@gmail.com>
2018-08-17 14:17:53 +0200

    Add IsItFive page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import Index from './Index.jsx';
 import Maps from './maps/Maps.jsx';
 import DiceIndex from './dice/DiceIndex.jsx';
 import Navigation from './Navigation.jsx';
+import IsItFive from './isitfive/IsItFive.jsx';
 
 export default class App extends React.Component {
   render() {
@@ -14,6 +15,7 @@ export default class App extends React.Component {
         <Route exact path="/" component={Index}/>
         <Route path="/maps" component={Maps}/>
         <Route path="/dice" component={DiceIndex}/>
+        <Route path="/isitfive" component={IsItFive}/>
       </div>
     );
   }
---
+++ src/components/isitfive/IsItFive.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Grid, Row, Col} from 'react-bootstrap';
+
+export default class IsItFive extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      date: new Date(),
+    };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => { this.tick(); }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({ date: new Date() });
+  }
+
+  render() {
+    return (
+      <div>
+        <Grid>
+          <Row>
+            <Col sm={12}>
+              <h1>Is it five already?</h1>
+            </Col>
+          </Row>
+          <br />
+          <Row>
+            <Col sm={12}>
+              <h2>
+                { (this.state.date.getHours() >= 17) ? "Yes, finally!" : "No, not yet" }
+              </h2>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    );
+  }
+}
