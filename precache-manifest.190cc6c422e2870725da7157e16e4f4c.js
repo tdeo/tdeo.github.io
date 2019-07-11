e8a1fde780ac88e0a528900a5bad80b2096434f7
Thierry Deo <thierry.deo@gmail.com>
2019-07-11 11:09:09 +0200

    Updates

---
+++ precache-manifest.190cc6c422e2870725da7157e16e4f4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd15850cfcd9127ad297040b58dc6471",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c1abedd2964b643e8aa",
+    "url": "/static/css/2.5db9248e.chunk.css"
+  },
+  {
+    "revision": "88d98ab1fd6cb361891c",
+    "url": "/static/css/main.44a79b8c.chunk.css"
+  },
+  {
+    "revision": "4c1abedd2964b643e8aa",
+    "url": "/static/js/2.5f9fbfc2.chunk.js"
+  },
+  {
+    "revision": "88d98ab1fd6cb361891c",
+    "url": "/static/js/main.eae31d76.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
