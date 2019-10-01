212f6674992fc8fb92ae2f126f0f2223a879a3e3
Thierry Deo <thierry.deo@gmail.com>
2019-10-01 18:36:59 +0000

    Updates

--- precache-manifest.addcfd61e6436388fb45db5fc515e61c.js
+++ precache-manifest.addcfd61e6436388fb45db5fc515e61c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2f67dd0854f11233856b93d55af43a4b",
+    "revision": "d08123e8c20ce3b0b739e4dbd91c4f65",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.e31a430a.chunk.css"
   },
   {
-    "revision": "6a5015e379a3cb8b3d39",
+    "revision": "3ecc667e01f4af273ebc",
     "url": "/static/css/main.7a772acf.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.aba0cb7d.chunk.js"
   },
   {
-    "revision": "6a5015e379a3cb8b3d39",
-    "url": "/static/js/main.b6acd015.chunk.js"
+    "revision": "3ecc667e01f4af273ebc",
+    "url": "/static/js/main.da0e12e4.chunk.js"
   },
   {
     "revision": "dfb1a0c1407bc3c4bf96",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e59ee876dbf920ec5550c5421ba951fb.js"
+  "/precache-manifest.addcfd61e6436388fb45db5fc515e61c.js"
 );
 
 self.addEventListener('message', (event) => {
